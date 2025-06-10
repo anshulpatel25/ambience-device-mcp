88dc47b29c7d819cd1aa81b52e7e636890ff5a4e
Anshul Patel <anshulpatel25@gmail.com>
2025-06-10 20:13:53 +0530

    Refactored to MCP server

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 import adafruit_dht
-import time
 
 from board import D14
+from mcp.server.fastmcp import FastMCP
 
+mcp = FastMCP("HomeWeather", host="0.0.0.0", port=3333)
 
-def main():
-    dht_device = adafruit_dht.DHT11(D14)
-
-    while True:
-        temperature = dht_device.temperature
-        humidity = dht_device.humidity
-        print(f"Temperature: {temperature} C, Humidity: {humidity} %")
-
-        time.sleep(5)
-
+@mcp.tool()
+async def get_home_weather(room: str) -> str:
+    """Get weather for room at home."""
+    dht_device = adafruit_dht.DHT11(D14, use_pulseio=False)
+    temperature = dht_device.temperature
+    humidity= dht_device.humidity
+    print(temperature, humidity)
+    return f"Temperature: {temperature} degree celcius, Humidity: {humidity} %"
 
 if __name__ == "__main__":
-    main()
+    mcp.run(transport="streamable-http")
